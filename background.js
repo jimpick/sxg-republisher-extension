2cd966c0959921aefa91790e453c3a6c1f2d623c
Jim Pick <jim@backblend.com>
2019-05-17 00:00:55 -0700

    Lookup sites via dnslink

--- background.js
+++ background.js
@@ -1,8 +1,67 @@
+const hostMappings = {}
+let redirectCount = 0
+let redirectTimestamp = 0
+
+const indexPage = 'https://index-jimpick.ipfs.v6z.me/'
+
+async function addMapping (host) {
+  const now = Date.now()
+  if (!hostMappings[host]) {
+    hostMappings[host] = { timestamp: 0 }
+  }
+  if (hostMappings[host].timestamp + 5000 < now) {
+    hostMappings[host].timestamp = now
+    delete hostMappings[host].error
+    console.log('addMapping', host, now)
+
+    const assetUrl = `https://ipfs.io/api/v0/dns/${host}?r=true`
+    const assetRes = await fetch(assetUrl)
+    const assetJson = await assetRes.json()
+    const assetPath = assetJson.Path
+    if (!assetPath) {
+      const errMsg = `Couldn't find dnslink for ${host}`
+      hostMappings[host].error = errMsg
+      console.error(errMsg)
+      return
+    }
+    const assetMatch = assetPath.match(/^\/ipfs\/(.*)$/)
+    if (!assetMatch) {
+      const errMsg = `No /ipfs/ in assetPath ${assetPath}`
+      hostMappings[host].error = errMsg
+      console.error(errMsg)
+      return
+    }
+    hostMappings[host].assetsCid = assetMatch[1]
+
+    const sxgUrl = `https://ipfs.io/api/v0/dns/sxg.${host}?r=true`
+    const sxgRes = await fetch(sxgUrl)
+    const sxgJson = await sxgRes.json()
+    const sxgPath = sxgJson.Path
+    if (!sxgPath) {
+      const errMsg = `Couldn't find dnslink for sxg.${host}`
+      hostMappings[host].error = errMsg
+      console.error(errMsg)
+      return
+    }
+    const sxgMatch = sxgPath.match(/^\/ipfs\/(.*)$/)
+    if (!sxgMatch) {
+      const errMsg = `No /ipfs/ in sxgPath ${sxgPath}`
+      hostMappings[host].error = errMsg
+      console.error(errMsg)
+      return
+    }
+    hostMappings[host].sxgCid = sxgMatch[1]
+
+    console.log('addMapping done', host, hostMappings[host])
+  }
+}
+
 chrome.webRequest.onBeforeRequest.addListener(
-  function(info) {
+  function (info) {
     console.log("Url: " + info.url)
 
     const { host, pathname } = new URL(info.url)
+    addMapping(host)
     let path = pathname
     if (path.endsWith('/')) {
       path += 'index.html'
@@ -10,77 +69,49 @@ chrome.webRequest.onBeforeRequest.addListener(
     if (!path.match(/\.[^.]+$/)) {
       path += '/index.html'
     }
-    /*
-    if (host === 'ipfs.v6z.me') {
-      const cid = 'QmUd1WPNTbiqDFpaGqTaUN16U6VAppqqjSQSi6KQxjjbfA'
+    const mapping = hostMappings[host]
+    const now = Date.now()
+    if (mapping && mapping.error && mapping.timestamp + 2000 > now) {
       return {
-        redirectUrl: `https://ipfs.jimpick.com/ipfs/${cid}${sxgPath}`
+        redirectUrl: 'data:text/html,' +
+        `Error: ${mapping.error}<br>` +
+        `Try again? <a href="${info.url}">${info.url}</a><br>` +
+        `Index page: <a href="${indexPage}">${indexPage}</a><br>`
       }
     }
-    if (host === 'tokyo.ipfs.v6z.me') {
-      const cid = 'QmR5i7DRxHQinAuCuJ1AVSQpft4QZUtQkSQWLcz7RbSCe5'
-      return {
-        redirectUrl: `https://ipfs.jimpick.com/ipfs/${cid}${sxgPath}`
+    if (
+      !mapping ||
+      !mapping.assetsCid ||
+      !mapping.sxgCid
+    ) {
+      if (redirectTimestamp + 2000 < now) {
+        redirectCount = 0
       }
-    }
-    if (host === 'vancouver.ipfs.v6z.me') {
-      const cid = 'QmSXovaJVapu1B5oxApghHCg5o7eVdxg5BGHyK92oCdquc'
-      return {
-        redirectUrl: `https://ipfs.jimpick.com/ipfs/${cid}${sxgPath}`
-      }
-    }
-    */
-    if (host === 'ipld-jimpick.ipfs.v6z.me') {
-      const cid = 'QmQCLv8ogu3t8t6HczbwpyJTGinmketphzE69z1nCsKkcJ'
-      const assetsCid = 'QmXb2bKQdgNhC7vaiKQgXFtt7daUZD382L54UTTNXnwQTD'
-      if (path.endsWith('.html')) {
+      redirectCount++
+      redirectTimestamp = now
+      if (redirectCount > 5) {
         return {
-          redirectUrl: `https://ipfs.io/ipfs/${cid}${path}.sxg`
+          redirectUrl: 'data:text/html,' +
+          `Failed to load<br>` +
+          `Try again? <a href="${info.url}">${info.url}</a><br>` +
+          `Index page: <a href="${indexPage}">${indexPage}</a><br>`
         }
       }
       return {
-        redirectUrl: `https://ipfs.io/ipfs/${assetsCid}${path}`
+        redirectUrl: 'data:text/html,' +
+          'Loading... ' + redirectCount +
+          '<script>' +
+          `setTimeout(() => document.location.href = "${info.url}", 1000);` +
+          '</script>'
       }
     }
-    if (host === 'peerpad-jimpick.ipfs.v6z.me') {
-      const cid = 'QmeeMqA8BAcAxQe58f3E886xS4JvNSRR3uz6CivPgsGMDS'
-      const assetsCid = 'QmWbsqqqG9YpNYDt5afp6HY8TrKMtCtdGUtUfgkS9fRYeH'
-      if (path.endsWith('.html')) {
-        return {
-          redirectUrl: `https://ipfs.io/ipfs/${cid}${path}.sxg`
-        }
-      }
+    if (path.endsWith('.html')) {
       return {
-        redirectUrl: `https://ipfs.io/ipfs/${assetsCid}${path}`
-      }
-    }
-    if (host === 'ipfs-docs-jimpick.ipfs.v6z.me') {
-      const cid = 'QmTXhjNmRiM5yL4mNs2bcq4MTZDkr1x5b8K2W41UdAGJqJ'
-      const assetsCid = 'QmXSHCEUJfFhGvw6U1aSuMtSTSnCmWcwvnCHdSZwbfRyer'
-      if (path.endsWith('.html')) {
-        return {
-          redirectUrl: `https://ipfs.io/ipfs/${cid}${path}.sxg`
-        }
-      }
-      return {
-        redirectUrl: `https://ipfs.io/ipfs/${assetsCid}${path}`
-      }
-    }
-    if (host === 'index-jimpick.ipfs.v6z.me') {
-      const cid = 'QmcgNj5wdUW5aRCdoimgFYqkkD2TLafTwzwjcudYpqGxeT'
-      const assetsCid = 'Qmcz7jdZPumWFdPFVJq7s3dWTZMFHtMVHyjurRthdMsNNi'
-      if (path.endsWith('.html')) {
-        return {
-          redirectUrl: `https://ipfs.io/ipfs/${cid}${path}.sxg`
-        }
-      }
-      return {
-        redirectUrl: `https://ipfs.io/ipfs/${assetsCid}${path}`
+        redirectUrl: `https://ipfs.io/ipfs/${mapping.sxgCid}${path}.sxg`
       }
     }
     return {
-      // redirectUrl: 'https://ipfs.io/ipfs/Qmcz7jdZPumWFdPFVJq7s3dWTZMFHtMVHyjurRthdMsNNi/'
-      redirectUrl: 'https://index-jimpick.ipfs.v6z.me/'
+      redirectUrl: `https://ipfs.io/ipfs/${mapping.assetsCid}${path}`
     }
   },
   // filters
